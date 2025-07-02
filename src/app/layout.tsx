fadf99e644dfdaab632f74d0bbb2b74b8a0aebd7
root <root@srvintranet2-go.go.trf1.gov.br>
2025-07-02 13:02:46 -0300

    ajustando layout para ssl rigoroso

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,5 @@
 import type { Metadata } from "next";
-import { Poppins } from "next/font/google";
 import "./globals.css";
-
-const poppins = Poppins({
-  subsets: ["latin"],
-  weight: ["400", "500", "600", "700"],
-  variable: "--font-poppins",
-});
 
 export const metadata: Metadata = {
   title: "Consulta Materiais",
